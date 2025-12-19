76d3f46e89ff2e873cca1a0c45621b1e62d06172
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2025-12-20 01:29:34 +0200

    Add WeatherDiseaseResult case class

--- src/main/scala/models/WeatherDiseaseResult.scala
+++ src/main/scala/models/WeatherDiseaseResult.scala
@@ -1,1 +1,13 @@
+package models
 
+case class WeatherDiseaseResult(
+                                 country: String,
+                                 season: String,
+                                 disease: String,
+                                 infection_rate: Double,
+                                 event_date: String,          
+                                 avg_temperature: Double,
+                                 avg_humidity: Double,
+                                 humidity_level: String,
+                                 weather_stress_index: Int
+                               )
